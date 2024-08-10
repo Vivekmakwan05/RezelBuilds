85d36b288d2a71d35ac97eedab66d675f5177174
vivek makwan <109407337+Vivekmakwan05@users.noreply.github.com>
2024-08-10 23:10:07 +0530

    Add files via upload

---
+++ js/form.js
@@ -0,0 +1,16 @@
+document.getElementById("btnn").addEventListener("click", function(){
+	document.querySelector(".popup").style.display = "flex";
+})
+
+document.querySelector(".close").addEventListener("click", function(){
+	document.querySelector(".popup").style.display = "none";
+})
+
+document.getElementById("btnn2").addEventListener("click", function(){
+	document.querySelector(".popup2").style.display = "flex";
+})
+
+document.querySelector(".close2").addEventListener("click", function(){
+	document.querySelector(".popup2").style.display = "none";
+})
+
